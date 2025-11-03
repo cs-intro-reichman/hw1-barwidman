bc4f80400eab14c88b3d464379db4e4d1ee7eb68
barwidman <bar.widman@post.runi.ac.il>
2025-11-03 18:50:59 +0200

    feat: Added solutions for Bill3 and FVCalc

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,19 @@
 // Splits a restaurant bill evenly among three diners.
+
 public class Bill3 {
+    public static final int NUM_OF_ARGS = 4;
 	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+
+        if (args.length != NUM_OF_ARGS) {
+            System.err.println("Usage: java Bill3 <name1> <name2> <name3> <TOTAL_PAYMENT>");
+            return;
+        }
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];
+        int totalPayment = Integer.parseInt(args[3]);
+        double toPayForEach = Math.ceil((double) totalPayment / 3);
+        String formatLine = String.format("Dear %s, %s, and %s: pay %.2f Shekels each.", name3, name2, name1, toPayForEach);
+        System.out.println(formatLine);
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,20 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
+    public static final int NUM_OF_ARGS = 3;
 	public static void main(String[] args){
-		// Replace this comment with your code
+
+        if (args.length != NUM_OF_ARGS) {
+            System.err.println("Usage: java FVCalc <CURRENT_VALUE> <INTEREST_RATE> <NUMBER_OF_YEARS>");
+            return;
+        }
+
+        int currentValue = Integer.parseInt(args[0]);
+        double interest = Double.parseDouble(args[1]);
+        double numYears = Integer.parseInt(args[2]);
+
+        double interestPercentage = interest / 100; // It is a percentage.
+
+        double valueAfterYears = currentValue * (Math.pow(1 + interestPercentage, (int)numYears));
+        System.out.println("After " + (int)numYears + " years, $" + currentValue + " saved at " + interest + "% will yield $" + (int)valueAfterYears);
 	}
 }
