96be8931cfdf0ba6b52d915e1506430bb35c02c8
barwidman <bar.widman@post.runi.ac.il>
2025-11-03 20:24:36 +0200

    feat: Added solution to Ascend exercise

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,17 @@
+import java.util.Random;
+
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+        int firstNumber = (int)(Math.random() * 100);
+        int secondNumber = (int)(Math.random() * 100);
+        int thirdNumber = (int)(Math.random() * 100);
+        int minValue = Math.min(Math.min(firstNumber, secondNumber),  thirdNumber);
+        int maxValue = Math.max(Math.max(firstNumber, secondNumber),  thirdNumber);
+        int middleValue = (firstNumber + secondNumber + thirdNumber) - minValue - maxValue;
+        System.out.println(firstNumber + " " + secondNumber + " " + thirdNumber);
+        System.out.println(minValue + " " + middleValue + " " + maxValue);
 	}
 }
